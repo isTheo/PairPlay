c3032531da3c756f9f7398fa13582141c096b96a
isTheo <matteohg07@gmail.com>
2024-12-05 20:29:32 +0100

    Added initial layout with background image, start button, and GameViewController.

--- PairPlay/GameViewController.swift
+++ PairPlay/GameViewController.swift
@@ -11,8 +11,9 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.systemOrange
+        
+        //grid here
     }
     
     
